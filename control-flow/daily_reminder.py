70d0540836b324d10dfeb68ea115810b02791089
Karim Tantawy <kareemtantawy01@gmail.com>
2024-12-22 23:35:18 +0200

    Added a task remider app based on priority & time-bound

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -10,29 +10,29 @@ match priority:
     case "high":
         if time_bound == "yes":
             print(
-                f"Remider: '{task}' is a {priority} priority task that requires immediate attention today!"
+                f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
             )
         else:
             print(
-                f"Remider: '{task}' is a {priority} priority task. Consider completing it when you have free time."
+                f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time."
             )
 
     case "medium":
         if time_bound == "yes":
             print(
-                f"Remider: '{task}' is a {priority} priority task that requires immediate attention today!"
+                f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
             )
         else:
             print(
-                f"Remider: '{task}' is a {priority} priority task. Consider completing it when you have free time."
+                f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time."
             )
 
     case "low":
         if time_bound == "yes":
             print(
-                f"Remider: '{task}' is a {priority} priority task that requires immediate attention today!"
+                f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
             )
         else:
             print(
-                f"Remider: '{task}' is a {priority} priority task. Consider completing it when you have free time."
+                f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time."
             )
