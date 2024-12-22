edb952016974d3c7d7e651925fd59e73f4ce279b
Karim Tantawy <kareemtantawy01@gmail.com>
2024-12-22 21:07:07 +0200

    Added the simple calculator app to apply to conditions

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,17 +7,13 @@ operatrion = input("Choose the operation (+, -, *, /): ")
 
 match operatrion:
     case "+":
-        result = num1 + num2
-        print(f"The result is: {result}.")
+        print(f"The result is: {num1 + num2}.")
     case "-":
-        result = num1 - num2
-        print(f"The result is: {result}.")
+        print(f"The result is: {num1 - num2}.")
     case "*":
-        result = num1 * num2
-        print(f"The result is: {result}.")
+        print(f"The result is: {num1 * num2}.")
     case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            result = num1 / num2
-            print(f"The result is: {result}.")
+            print(f"The result is: {num1 / num2}.")
