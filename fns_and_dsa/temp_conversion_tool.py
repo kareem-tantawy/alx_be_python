3558dfd17c0bd68ca63601c53cb62bac41254918
Karim Tantawy <kareemtantawy01@gmail.com>
2025-01-12 23:44:58 +0200

    Simple app to convert from convert to fahrenheit and vice versa

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,27 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 0
+CELSIUS_TO_FAHRENHEIT_FACTOR = 0
+
+
+def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9 * (fahrenheit - 32)
+    return FAHRENHEIT_TO_CELSIUS_FACTOR
+
+
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5 * celsius + 32
+    return CELSIUS_TO_FAHRENHEIT_FACTOR
+
+    """Enter the temperature to convert: 100
+Is this temperature in Celsius or Fahrenheit? (C/F): F
+100.0°F is 37.77777777777778°C
+    """
+
+
+temperature = float(input("Enter the temperature to convert: "))
+F_or_C = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+if F_or_C == "F":
+    print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
+else:
+    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
