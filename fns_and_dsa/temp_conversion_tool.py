18020db43a01212137334b4b1e28b9889f10c8cf
Karim Tantawy <kareemtantawy01@gmail.com>
2025-01-13 00:00:45 +0200

    Simple app to convert from convert to fahrenheit and vice versa

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,6 +2,7 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 
+
 def convert_to_celsius(fahrenheit):
 
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -12,7 +13,6 @@ def convert_to_fahrenheit(celsius):
 
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
-
 
 def main():
 
