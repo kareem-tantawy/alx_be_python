11f6151936ec4d2e582ac4c118232c768fa17573
Karim Tantawy <kareemtantawy01@gmail.com>
2025-01-12 22:46:17 +0200

    Basic calculator function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2, operation):
             return num1 - num2
     elif operation == "multiply":
             return num1 * num2
-    else
+    else:
             if num2 != 0:
                 return num1 / num2
             else:
