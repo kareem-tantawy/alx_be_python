a54f8c61ba3df00c5fcf1b5c450deb6d070040cf
Karim Tantawy <kareemtantawy01@gmail.com>
2025-01-18 04:01:56 +0200

    Basic bank account class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,8 +10,14 @@ class BankAccount:
     def withdraw(self, withdraw_amount):
         if self.__amount > withdraw_amount:
             self.__amount -= withdraw_amount
+            print(f"Withdrew: {withdraw_amount}")
         else:
             print("Insufficient funds.")
 
     def display_balance(self):
         print(f"Current Balance: {self.__amount}")
+
+
+# bank = BankAccount(100)
+# bank.withdraw(20)
+# bank.display_balance()
