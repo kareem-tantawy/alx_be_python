1f1679072257351a72c92c7b7b7d63ca1aa61f2f
Karim Tantawy <kareemtantawy01@gmail.com>
2025-01-18 03:49:19 +0200

    Basic bank account class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAcount:
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def deposit(self, deposit_amount):
+        self.__amount += deposit_amount
+
+    def withdraw(self, withdraw_amount):
+        if self.__amount > withdraw_amount:
+            self.__amount -= withdraw_amount
+        else:
+            print("Insufficient funds.")
+
+    def display_balance(self):
+        print(self.__amount)
