f145a138049a7c570f9121c1b7e281a195d054e4
Karim Tantawy <kareemtantawy01@gmail.com>
2025-01-19 20:23:15 +0200

    unit test on simple calculator class that contain the four main operations in math

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,42 +1,28 @@
 import unittest
-# from simple_calculator import SimpleCalculator
-
-
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
-
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
-
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
-
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
-
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
+from simple_calculator import SimpleCalculator
 
 
 class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
     def test_add(self):
-        self.assertAlmostEqual(SimpleCalculator.add(self, 1, 2), 3)
-        self.assertAlmostEqual(SimpleCalculator.add(self, -2, -4), -6)
+        self.assertEqual(self.calc.add(self, 1, 2), 3)
+        self.assertEqual(self.calc.add(self, -2, -4), -6)
+        self.assertEqual(self.calc.add(self, -2, 2), 0)
 
     def test_subtract(self):
-        self.assertAlmostEqual(SimpleCalculator.subtract(self, -5, 4), -9)
-        self.assertAlmostEqual(SimpleCalculator.subtract(self, 12, 2), 10)
+        self.assertEqual(self.calc.subtract(self, -5, 4), -9)
+        self.assertEqual(self.calc.subtract(self, 12, 2), 10)
+        self.assertEqual(self.calc.subtract(self, -12, -5), -7)
 
     def test_multiply(self):
-        self.assertAlmostEqual(SimpleCalculator.multiply(self, 5, 5), 25)
-        self.assertAlmostEqual(SimpleCalculator.multiply(self, 5, 0), 0)
+        self.assertEqual(self.calc.multiply(self, 5, 5), 25)
+        self.assertEqual(self.calc.multiply(self, 5, 0), 0)
 
     def test_divide(self):
-        self.assertAlmostEqual(SimpleCalculator.divide(self, 10, 2), 5)
-        self.assertAlmostEqual(SimpleCalculator.divide(self, 10, 0), None)
+        self.assertEqual(self.calc.divide(self, 10, 2), 5)
+        self.assertEqual(self.calc.divide(self, 2, 10), 0.2)
+        self.assertEqual(self.calc.divide(self, 10, 0), None)
